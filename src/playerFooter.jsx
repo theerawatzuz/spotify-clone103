4545821d4e302eefec610b8e0f9eb7181459e406
TheerawatNB <bankt21@gmail.com>
2024-04-12 03:00:37 +0700

    ClearFlex-1

--- src/playerFooter.jsx
+++ src/playerFooter.jsx
@@ -1,12 +1,21 @@
+import play from './assets/icons/play.png'
+import pause from './assets/icons/pause.png'
+import React, { useState } from 'react';
 
 export default function PlayerFooter() {
+
+    const [isPlaying, setIsPlaying] = useState(false);
+
+    const togglePlayPause = () => {
+      setIsPlaying(!isPlaying);
+    };
   return (
     <>
     <nav class="bg-zinc-950">
   <div class="mx-auto">
-    <div class="container relative flex h-24 items-center text-white justify-between p-7">
-        <div class="col left">
-              <div class="gap-2 item-center flex flex-col rounded-lg text-surface shadow-secondary-1 dark:bg-surface-dark text-white md:max-w-xl md:flex-row ">
+    <div class="flex justify-between h-24 items-center text-white p-7">
+        <div class="flex-1 grid justify-items-start">
+              <div class="flex gap-2 item-center rounded-lg text-white">
                   <img
                     class="h-16 w-16 rounded-lg object-cover "
                     src="https://tecdn.b-cdn.net/wp-content/uploads/2020/06/vertical.jpg"
@@ -22,8 +31,20 @@ export default function PlayerFooter() {
                   </div>
                 </div>
               </div>
-        <div class="col center"> 123</div>
-        <div class="col right"> 123</div>
+         <div className="flex-1 grid justify-items-center cursor-pointer" onClick={togglePlayPause}>
+                {isPlaying ? (
+                <img src={pause} alt="Pause" />
+                ) : (
+                <img src={play} alt="Play" />
+                )}
+            </div>
+            <div className="flex-1 grid justify-items-end cursor-pointer" onClick={togglePlayPause}>
+                {isPlaying ? (
+                <img src={pause} alt="Pause" />
+                ) : (
+                <img src={play} alt="Play" />
+                )}
+            </div>
     </div>
   </div>
 
